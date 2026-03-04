24c4e563a7ad239af1e9373cc99b84ec2f3a470b
Valery Kaplich <163750665+personanongraat@users.noreply.github.com>
2026-03-04 23:25:44 +0900

    Add delete function
    Added delete functionality to remove the last entry from bitcoin data. Improved input validation for buy price and amount.

--- functional.js
+++ functional.js
@@ -1,12 +1,32 @@
 const buyPriceInput = document.getElementById("buyPrice");
 const amountInput = document.getElementById("amount");
 const saveBtn = document.getElementById("saveBtn");
+const deleteBtn = document.getElementById("deleteBtn");
 const resultDiv = document.getElementById("result");
 let bitcoinData = [];
+bitcoinData.push(
+  { amount: 0.010057, buyPrice: 87700 },
+  { amount: 0.001388, buyPrice: 72000 },
+  { amount: 0.006702, buyPrice: 71776 },
+  { amount: 0.007592, buyPrice: 67000 },
+  { amount: 0.007311, buyPrice: 68526 },
+  { amount: 0.010044, buyPrice: 78000 },
+  { amount: 0.007485, buyPrice: 66801 },
+  { amount: 0.007173, buyPrice: 69770 },
+  { amount: 0.007395, buyPrice: 67520 },
+  { amount: 0.006933, buyPrice: 66270 },
+  { amount: 0.015793, buyPrice: 63240 },
+  { amount: 0.011967, buyPrice: 64235 },
+);
+showOutput();
 
 function saveData() {
-  const buyPrice = buyPriceInput.value;
-  const amount = amountInput.value;
+  const buyPrice = parseFloat(buyPriceInput.value);
+  const amount = parseFloat(amountInput.value);
+  if (isNaN(buyPrice) || isNaN(amount)) {
+    alert("Заполните поля");
+    return;
+  }
   const data = {
     buyPrice: buyPrice,
     amount: amount,
@@ -15,16 +35,16 @@ function saveData() {
   localStorage.setItem("bitcoinData", JSON.stringify(bitcoinData));
   showOutput();
 }
+function deleteData() {
+  bitcoinData.pop();
+  showOutput();
+}
 
 function showOutput() {
   resultDiv.innerHTML = "";
   bitcoinData.forEach(function (item, index) {
     resultDiv.innerHTML +=
-      " amount: " +
-      item.buyPrice +
-      " price: " +
-      item.amount +
-      " $ <br>";
+      " amount: " + item.buyPrice + " price: " + item.amount + " $ <br>";
   });
 }
 
@@ -36,6 +56,6 @@ window.onload = function () {
     showOutput();
   }
 };
-
+deleteBtn.addEventListener("click", deleteData);
 saveBtn.addEventListener("click", saveData);
 
